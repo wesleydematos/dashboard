9380a7fd0ddf6495b4f50290b8fa71c7a26b24c2
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-02 12:10:52 -0300

    chore: added react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { GlobalStyle } from "./styles/global.js"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
