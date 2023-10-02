53ff2b7a4d2863b50560fd8cb4accce59c6659e9
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-02 18:06:08 -0300

    feat: created todays sales

--- src/components/TodaySales/index.jsx
+++ src/components/TodaySales/index.jsx
@@ -1,6 +1,49 @@
+import { StyledTodaySales } from "./style"
+import exportimg from "@/assets/export.png"
+import salesicon from "@/assets/salesicon.png"
+import ordericon from "@/assets/ordericon.png"
+import discicon from "@/assets/discicon.png"
+
 function TodaySales() {
     return (
-      <p>today sales</p>
+      <StyledTodaySales>
+        <div className="texts">
+          <div>
+            <p>Today&apos;s Sales</p>
+            <span>Sales Summary</span>
+          </div>
+          <button>
+            <img src={exportimg} alt="Export"/>
+            Export
+          </button>
+        </div>
+        <div className="cards">
+          <div>
+            <div>
+              <img src={salesicon} alt="Sales icon" />
+            </div>
+            <h2>$1k</h2>
+            <p>Total Sales</p>
+            <span>+8% from yesterday</span>
+          </div>
+          <div>
+            <div>
+              <img src={ordericon} alt="Order Icon" />
+            </div>
+            <h2>300</h2>
+            <p>Total Order</p>
+            <span>+5% from yesterday</span>
+          </div>
+          <div>
+            <div>
+              <img src={discicon} alt="Disc Icon" />
+            </div>
+            <h2>5</h2>
+            <p>Product Sold</p>
+            <span>+1.2% from yesterday</span>
+          </div>
+        </div>
+      </StyledTodaySales>
     )
   }
   
---
+++ src/components/TodaySales/style.js
@@ -0,0 +1,131 @@
+import styled from "styled-components"
+
+export const StyledTodaySales = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 348px;
+    width: 664px;
+    padding: 20px;
+    margin: 20px;
+    background-color: var(--white-bg);
+    border-radius: 10px;
+
+    .texts{
+        display: flex;
+        justify-content: space-between;
+        margin-top: 10px;
+
+        div{
+            display: flex;
+            flex-direction: column;
+
+            p{
+                font-weight: bold;
+                font-size: 20px;
+            }
+
+            span{
+                font-weight: 400;
+                font-size: 16px;
+                color: var(--text-gray);
+            }
+        }
+
+        button{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid var(--text-gray);
+            background-color: var(--white-bg);
+            border-radius: 10px;
+            font-weight: 500;
+            font-size: 14px;
+            height: 39px;
+            width: 99px;
+            gap: 5px;
+
+            img{
+                height: 16px;
+                width: 16px;
+            }
+        }
+    }
+
+    .cards{
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 15px;
+
+        > div{
+            margin-top: 15px;
+            width: 180px;
+            height: 184px;
+            border-radius: 10px;
+            padding: 0 15px;
+        }
+
+        > :nth-child(1){
+            background-color: var(--pink-2);
+
+            div{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 40px;
+                width: 40px;
+                margin: 10px 0;
+                border-radius: 50%;
+                background-color: var(--pink-1);
+            }
+        }
+
+        > :nth-child(2){
+            background-color: var(--orange-2);
+
+            div{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 40px;
+                width: 40px;
+                margin: 10px 0;
+                border-radius: 50%;
+                background-color: var(--orange-1);
+            }
+        }
+
+        > :nth-child(3){
+            background-color: var(--green-2);
+
+            div{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 40px;
+                width: 40px;
+                margin: 10px 0;
+                border-radius: 50%;
+                background-color: var(--green-1);
+            }
+        }
+
+        h2{
+            font-weight: bold;
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        p{
+            font-weight: 500;
+            font-size: 16px;
+            color: var(--text-gray);
+            margin-bottom: 10px;
+        }
+
+        span{
+            font-size: 12px;
+            color: var(--blue-1);
+        }
+    }
+`
