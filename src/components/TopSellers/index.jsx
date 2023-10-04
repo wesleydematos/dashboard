c64a2f51b6fdedfa4d5191473ffd6d07d26e9f1e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-04 13:49:55 -0300

    feat: created top sellers

--- src/components/TopSellers/index.jsx
+++ src/components/TopSellers/index.jsx
@@ -1,6 +1,68 @@
+import { StyledTopSellers } from "./style"
+
 function TopSellers() {
     return (
-      <p>top sellers</p>
+      <StyledTopSellers>
+        <p>Top Sellers</p>
+        <ul>
+          <li>
+            <span>#</span>
+            <span>Name</span>
+            <span>Popularity</span>
+            <span>Sales</span>
+          </li>
+          <li>
+            <span>01</span>
+            <p>Nome</p>
+            <div>
+              <div>
+                <div/>
+              </div>
+            </div>
+            <p>34%</p>
+          </li>
+          <li>
+            <span>02</span>
+            <p>Nome</p>
+            <div>
+              <div>
+                <div/>
+              </div>
+            </div>
+            <p>25%</p>
+          </li>
+          <li>
+            <span>03</span>
+            <p>Nome</p>
+            <div>
+              <div>
+                <div/>
+              </div>
+            </div>
+            <p>20%</p>
+          </li>
+          <li>
+            <span>04</span>
+            <p>Nome</p>
+            <div>
+              <div>
+                <div/>
+              </div>
+            </div>
+            <p>13%</p>
+          </li>
+          <li>
+            <span>05</span>
+            <p>Nome</p>
+            <div>
+              <div>
+                <div/>
+              </div>
+            </div>
+            <p>8%</p>
+          </li>
+        </ul>
+      </StyledTopSellers>
     )
   }
   
