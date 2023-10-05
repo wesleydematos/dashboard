141a0bf2064f6b9915de077d6266a068bb46e83d
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-05 16:21:32 -0300

    feat: added new responsive screen

--- src/components/TopSellers/style.js
+++ src/components/TopSellers/style.js
@@ -3,9 +3,6 @@ import styled from "styled-components"
 export const StyledTopSellers = styled.div`
     display: flex;
     flex-direction: column;
-    /* height: 355px;
-    width: 1117px;
-    padding: 20px; */
     height: 240px;
     width: 350px;
     padding: 20px;
@@ -15,25 +12,21 @@ export const StyledTopSellers = styled.div`
     > p{
         font-weight: bold;
         font-size: 12px;
-        /* font-size: 20px; */
     }
 
     ul{
         display: flex;
         flex-direction: column;
         margin-top: 5px;
-        /* margin-top: 15px; */
 
         li{
             display: flex;
             border-bottom: 1px solid #edf2f6;
             padding-bottom: 5px;
-            /* padding-bottom: 13px; */
         }
 
         > :nth-child(1){
             span{
-                /* font-size: 16px; */
                 font-size: 12px;
                 font-weight: 400;
                 color: var(--text-gray);
@@ -41,17 +34,11 @@ export const StyledTopSellers = styled.div`
 
             > :nth-child(1){
                 width: 20px;
-                /* width: 70px; */
             }
 
             > :nth-child(2), >:nth-child(3){
                 width: 150px;
-                /* width: 370px; */
             }
-
-            /* >:nth-child(3){
-                margin-right: 35px;
-            } */
         }
 
         > :nth-child(2), > :nth-child(3), > :nth-child(4), > :nth-child(5), > :nth-child(6){
@@ -60,17 +47,13 @@ export const StyledTopSellers = styled.div`
             font-size: 10px;
             font-weight: 400;
             padding: 6px 0;
-            /* font-size: 14px;
-            padding: 13px 0; */
 
             span{
                 width: 20px;
-                /* width: 70px; */
             }
 
             > :nth-child(2){
                 width: 120px;
-                /* width: 370px; */
             }
 
             > :nth-child(3){
@@ -78,23 +61,15 @@ export const StyledTopSellers = styled.div`
                 height: 14px;
                 display: flex;
                 align-items: center;
-                /* height: 24px; */
-                /* width: 370px; */
 
                 div{
                     width: 100%;
                     height: 4px;
                     border-radius: 10px;
-                    /* margin-right: 95px; */
                 }
             }
 
             > :last-child{
-                /* width: 85px; */
-                /* height: 24px; */
-                /* font-size: 14px; */
-                /* border-radius: 10px; */
-                /* margin-left: 0; */
                 display: flex;
                 justify-content: center;
                 align-items: center;
@@ -203,10 +178,123 @@ export const StyledTopSellers = styled.div`
                 background-color: var(--pink-2);
             }
         }
+    }
 
-        @media (min-width: 768px) {
+    @media (min-width: 768px) {
+        height: 315px;
+        width: 660px;
+        padding: 15px;
+
+        >p{
+            font-size: 20px;
+        }
+
+        ul{
+            margin-top: 10px;
             
+            li{
+                padding-bottom: 10px;
+            }
+
+            > :nth-child(1){
+                span{
+                    font-size: 14px;
+                }
+
+                > :nth-child(1){
+                    width: 45px;
+                }
+                
+                > :nth-child(2), >:nth-child(3){
+                    width: 220px;
+                }
+                
+                >:nth-child(3){
+                    margin-right: 25px;
+                }
+            }
+
+            > :nth-child(2), > :nth-child(3), > :nth-child(4), > :nth-child(5), > :nth-child(6){
+                font-size: 12px;
+                padding: 10px 0;
+                
+                span{
+                    width: 45px;
+                }
+
+                > :nth-child(2){
+                    width: 210px;
+                }
+
+                > :nth-child(3){
+                    height: 24px; 
+                    width: 220px;
+                    margin-right: 25px;
+                }
+
+                :last-child{
+                    width: 85px;
+                    height: 24px;
+                    font-size: 12px;
+                    border-radius: 10px;
+                    margin-left: 0;
+                }
+            }
         }
     }
 
+    @media (min-width: 1485px) {
+        height: 355px;
+        width: 1100px;
+        padding: 20px;
+
+        ul{
+            margin-top: 15px;
+            
+            li{
+                padding-bottom: 13px;
+            }
+
+            > :nth-child(1){
+                span{
+                    font-size: 16px;
+                }
+
+                > :nth-child(1){
+                    width: 70px;
+                }
+
+                > :nth-child(2), >:nth-child(3){
+                    width: 370px;
+                }
+
+                >:nth-child(3){
+                    margin-right: 95px;
+                }
+            }
+
+            > :nth-child(2), > :nth-child(3), > :nth-child(4), > :nth-child(5), > :nth-child(6){
+                font-size: 14px;
+                padding: 13px 0;
+                
+                span{
+                    width: 70px;
+                }
+
+                > :nth-child(2){
+                    width: 370px;
+                }
+
+                > :nth-child(3){
+                    width: 370px;
+                    margin-right: 95px;
+                }
+
+                :last-child{
+                    font-size: 14px;
+                    border-radius: 10px;
+                }
+            }
+        }
+    }
 `
